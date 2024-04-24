958c9aa4b1ca445df0db178614b36dad18ab4473
k1gaa <k1gaa@icloud.com>
2024-04-24 19:52:09 +0200

    added managment of tasks function

--- components/TodoItem/TodoItem.jsx
+++ components/TodoItem/TodoItem.jsx
@@ -1,9 +1,19 @@
 import styles from "./TodoItem.module.css";
 
-export function TodoItem({ name, done }) {
+export function TodoItem({ name, done, onDoneSubmit, onRemoveSubmit }) {
   return (
     <li className={styles.list}>
-      {name} {done}
+      <p className={`${done && styles["done-list"]}`}>{name}</p>
+      <div className={styles.container}>
+        {!done && (
+          <button onClick={onDoneSubmit} className={styles.done__btn}>
+            Done
+          </button>
+        )}
+        <button onClick={onRemoveSubmit} className={styles.remove__btn}>
+          Remove
+        </button>
+      </div>
     </li>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { TodoItem } from "../components/TodoItem/TodoItem";
 import styles from "./App.module.css";
 import { useState } from "react";
 
-const todosArray = [
-  {
-    name: "wash dishes",
-    done: true,
-    id: 1,
-  },
-  {
-    name: "go to shop",
-    done: true,
-    id: 2,
-  },
-];
-
 function App() {
+  const [todosArray, setTodosArray] = useState([
+    {
+      name: "wash dishes",
+      done: true,
+      id: 1,
+    },
+    {
+      name: "go to shop",
+      done: false,
+      id: 2,
+    },
+  ]);
+
   const [isFormShown, setIsFormShown] = useState(false);
 
   return (
@@ -37,12 +37,43 @@ function App() {
       {isFormShown && (
         <form className={styles.form}>
           <input type="text" className={styles.input} />
-          <button className={styles.add__btn}>Add</button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className={styles.add__btn}
+          >
+            Add
+          </button>
         </form>
       )}
       <ul>
         {todosArray.map(({ name, done, id }) => {
-          return <TodoItem name={name} done={done} key={id} />;
+          return (
+            <TodoItem
+              name={name}
+              done={done}
+              key={id}
+              onDoneSubmit={() => {
+                setTodosArray((prevTodos) =>
+                  prevTodos.map((todo) => {
+                    if (todo.id !== id) {
+                      return todo;
+                    }
+                    return {
+                      ...todo,
+                      done: true,
+                    };
+                  })
+                );
+              }}
+              onRemoveSubmit={() => {
+                setTodosArray((prevTodos) =>
+                  prevTodos.filter((todo) => todo.id !== id)
+                );
+              }}
+            />
+          );
         })}
       </ul>
     </div>
